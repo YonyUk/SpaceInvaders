63ea499953d2d6a5dad5c7889107c89d9b73831e
YonyUk <yonatanjoseguerraperez@gmail.com>
2024-12-12 03:06:18 +0100

    Add files via upload
    first commit

---
+++ Assets/Scripts/Architecture/IShipStructure.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Architecture{
+
+	public interface IShipStructure:IStructure{
+		IShooter Shooter { get; }
+		float Speed { get; }
+		float MaxSpeed { get; }
+	}
+}
